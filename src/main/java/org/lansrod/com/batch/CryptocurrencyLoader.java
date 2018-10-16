6459765dcf85dc4010ef3a7520d639ff34d57011
Omran Toujani <omrane.toujani@tcb-consulting.net>
2018-10-16 13:09:30 +0200

    restructure

---
+++ src/main/java/org/lansrod/com/batch/CryptocurrencyLoader.java
@@ -0,0 +1,8 @@
+package src.main.java.org.lansrod.stock.batch;
+
+import org.lansrod.stock.batch.AbstractLoader;
+
+public class CryptocurrencyLoader extends AbstractLoader{
+
+}
+																		
